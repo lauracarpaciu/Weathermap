80f56cab4d62a65d942caa97d09a1aeed762c969
Mirela <lauracarpaciu>
2022-09-15 11:43:31 +0300

    annotation.RestController

--- demo/demo-backend/src/main/java/com/lauracarpaciu/demo/controller/OwmController.java
+++ demo/demo-backend/src/main/java/com/lauracarpaciu/demo/controller/OwmController.java
@@ -14,11 +14,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.lauracarpaciu.demo.service.WeatherService;
 
 
-@Controller
+@RestController
 @RequestMapping("/api")
 public class OwmController {
 	
